33602f8c13467216be8b63f1fdd0553abe2530a3
peang <pngo93@gmail.com>
2024-03-11 17:07:19 -0500

    similar string manipulation to JS

---
+++ strings.java
@@ -0,0 +1,14 @@
+String greeting = "Hello World"
+System.out.println(greeting)
+
+//String Length
+//A String in Java is actually an object, which contain methods that can perform certain operations on strings. For example, the length of a string can be found with the length() method:
+
+String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+System.out.println("The length of the txt string is: " + txt.length());
+
+
+System.out.println(greeting.toUpperCase());
+System.out.println(greeting.toLowerCase());
+
+System.out.println(greeting.indexOf("World")) // Outputs 6
