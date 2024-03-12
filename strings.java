baa44a3af72288655524d3f424c412b86b63d3a9
peang <pngo93@gmail.com>
2024-03-12 12:07:53 -0500

    strings with special chars

--- strings.java
+++ strings.java
@@ -18,3 +18,33 @@ String firstName = "John";
 String lastName = "Doe";
 System.out.println(firstName + " " + lastName)
 System.out.println(lastName.concat(firstName))
+
+
+
+String x = "10";
+int y = 20;
+String z = x + y; // Outputs z will be 1020 (a string)
+
+
+/*
+The backslash (\) escape character turns special characters into string characters:
+
+Escape character	Result	Description
+\'	'	Single quote
+\"	"	Double quote
+\\	\	Backslash
+The sequence \"  inserts a double quote in a string:
+
+ */
+
+
+ // The sequence \| inserts a double quote in a string
+ String txt = "We are the so-called \"Vikings\" from the north. "
+
+ // The sequence \' inserts a single quote in a string:
+
+ String txt2 = "It\'s alright.";
+
+ // The sequence \\ inserts a single backslash in a string:
+
+ String txt3 = "The character \\ is called backslash.";
